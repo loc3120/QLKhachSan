826929eae2e4b7d166bc4805ff63f80ac2668690
Nguyễn Lộc <loc3120@gmail.com>
2021-04-29 01:08:59 +0700

    Thêm mới project

---
+++ Database/Table and Data.sql
@@ -0,0 +1,150 @@
+﻿USE [QLKhachSan]
+
+CREATE TABLE KHACHHANG (
+	MaKH CHAR(6) PRIMARY KEY,
+	HoTen NVARCHAR(50),
+	NgaySinh DATE,
+	GioiTinh NVARCHAR(5) CHECK (GioiTinh in (N'Nam', N'Nữ')),
+)
+
+CREATE TABLE PHONG (
+	MaPhong CHAR(6) PRIMARY KEY,
+	TenPhong VARCHAR(10),
+	LoaiPhong NVARCHAR(50),
+	GiaThue1Ngay INT
+)
+
+CREATE TABLE HOADON (
+	MaHD CHAR(6) PRIMARY KEY,
+	NgayBatDauThue DATE,
+	NgayThu DATE,
+	TongTien INT,
+	MaKH CHAR(6) REFERENCES KHACHHANG(MaKH),
+	MaPhong CHAR(6) REFERENCES PHONG(MaPhong)
+)
+
+CREATE TABLE VATDUNG(
+	MaVatDung CHAR(6) PRIMARY KEY,
+	TenVatDung NVARCHAR(50),
+	GiaTienSuDung INT,
+	MaPhong CHAR(6) REFERENCES PHONG(MaPhong)
+)
+
+CREATE TABLE HOADON_VATDUNG(
+	MaHD CHAR(6) REFERENCES HOADON(MaHD),
+	MaVatDung CHAR(6) REFERENCES VATDUNG(MaVatDung),
+	SoLuong INT,
+	TongTienSuDungVatDung INT,
+	PRIMARY KEY (MaHD, MaVatDung)
+)
+
+CREATE TABLE DICHVU(
+	MaDV CHAR(6) PRIMARY KEY,
+	TenDV NVARCHAR(50),
+	GiaSuDung INT
+)
+
+CREATE TABLE HOADON_DICHVU(
+	MaHD CHAR(6) REFERENCES HOADON(MaHD),
+	MaDV CHAR(6) REFERENCES DICHVU(MaDV),
+	SoLanSuDungDichVu INT,
+	TongTienSuDungDichVu INT,
+	PRIMARY KEY(MaHD, MaDV)
+)
+
+--DROP TABLE HOADON_DICHVU
+--DROP TABLE HOADON_VATDUNG
+--DROP TABLE DICHVU
+--DROP TABLE VATDUNG
+--DROP TABLE HOADON
+--DROP TABLE KHACHHANG
+--DROP TABLE PHONG
+
+INSERT INTO KHACHHANG VALUES
+	('KH0001', N'Nguyễn Thị Thanh Tùng', '2000/11/03', N'Nữ'),
+	('KH0002', N'Bùi Doãn Tú', '2000/08/25', N'Nam'),
+	('KH0003', N'Nguyễn Minh Kiên', '2000/02/13', N'Nam'),
+	('KH0004', N'Mạc Trung Lam', '2000/08/03', N'Nam'),
+	('KH0005', N'Vũ Anh Hiếu', '2000/01/30', N'Nam'),
+	('KH0006', N'Nguyễn Hoàng Hưng', '2000/12/12', N'Nam'),
+	('KH0007', N'Bùi Thị Mỹ Hưng', '2000/06/25', N'Nữ'),
+	('KH0008', N'Đỗ Thành Thằng Tùng', '2000/08/10', N'Nam'),
+	('KH0009', N'Kiều Thị Như Kiên', '2000/02/29', N'Nam'),
+	('KH0010', N'Đô Nan Lộc', '2000/03/15', N'Nam')
+
+INSERT INTO PHONG VALUES
+	('PH0001', 'P101', N'Vip Pro No 1', 700000),
+	('PH0002', 'P102', N'Tình yêu', 1000000),
+	('PH0003', 'P201', N'Chó Shiba', 500000),
+	('PH0004', 'P202', N'Siêu cấp vũ trụ', 700000),
+	('PH0005', 'P301', N'Top Server', 1700000)
+
+INSERT INTO HOADON(MaHD, MaKH, MaPhong) VALUES
+	('HD0001', 'KH0001', 'PH0002'),
+	('HD0002', 'KH0002', 'PH0001'),
+	('HD0003', 'KH0003', 'PH0002'),
+	('HD0004', 'KH0004', 'PH0004'),
+	('HD0005', 'KH0005', 'PH0003'),
+	('HD0006', 'KH0006', 'PH0005'),
+	('HD0007', 'KH0007', 'PH0001'),
+	('HD0008', 'KH0008', 'PH0004'),
+	('HD0009', 'KH0009', 'PH0003'),
+	('HD0010', 'KH0010', 'PH0005')
+
+INSERT INTO VATDUNG VALUES
+	('VD0001', N'Tủ lạnh mini', 120000, 'PH0001'),
+	('VD0002', N'Cô ca', 10000, 'PH0001'),
+	('VD0003', N'Nước lọc', 6000, 'PH0002'),
+	('VD0004', N'Bao cao su', 50000, 'PH0003'),
+	('VD0005', N'Khăn tắm', 30000, 'PH0002'),
+	('VD0006', N'Xới cơm', 120000, 'PH0003'),
+	('VD0007', N'Dây trói', 120000, 'PH0003'),
+	('VD0008', N'Ghế tình yêu', 120000, 'PH0003'),
+	('VD0009', N'Tủ lạnh mini', 120000, 'PH0004'),
+	('VD0010', N'Pepsi', 120000, 'PH0004'),
+	('VD0011', N'Nước lọc', 120000, 'PH0004'),
+	('VD0012', N'Khăn tắm', 120000, 'PH0004'),
+	('VD0013', N'Xới cơm', 120000, 'PH0005'),
+	('VD0014', N'Cô ca', 120000, 'PH0005'),
+	('VD0015', N'Bao cao su', 120000, 'PH0005')
+
+INSERT INTO HOADON_VATDUNG(MaHD, MaVatDung, SoLuong) VALUES
+	('HD0001', 'VD0001', 1),
+	('HD0002', 'VD0002', 3),
+	('HD0003', 'VD0003', 2),
+	('HD0004', 'VD0004', 4),
+	('HD0003', 'VD0005', 5),
+	('HD0002', 'VD0005', 3),
+	('HD0001', 'VD0003', 6),
+	('HD0005', 'VD0004', 3),
+	('HD0006', 'VD0007', 4),
+	('HD0003', 'VD0006', 5),
+	('HD0006', 'VD0008', 2),
+	('HD0002', 'VD0009', 3),
+	('HD0002', 'VD0010', 4),
+	('HD0007', 'VD0012', 5),
+	('HD0008', 'VD0011', 3),
+	('HD0009', 'VD0013', 1),
+	('HD0008', 'VD0014', 2),
+	('HD0010', 'VD0015', 3),
+	('HD0010', 'VD0010', 5),
+	('HD0007', 'VD0015', 4)
+
+INSERT INTO DICHVU VALUES
+	('DV0001', N'Giặt là', 30000),
+	('DV0002', N'Đưa đón', 30000),
+	('DV0003', N'Ăn sáng', 30000),
+	('DV0004', N'Ăn trưa', 30000),
+	('DV0005', N'Ăn tối', 30000)
+
+INSERT INTO HOADON_DICHVU(MaHD, MaDV, SoLanSuDungDichVu) VALUES
+	('HD0001', 'DV0001', 3 ),
+	('HD0002', 'DV0002', 2 ),
+	('HD0003', 'DV0002', 4 ),
+	('HD0004', 'DV0003', 6 ),
+	('HD0005', 'DV0004', 2 ),
+	('HD0006', 'DV0003', 3 ),
+	('HD0007', 'DV0005', 4 ),
+	('HD0008', 'DV0001', 2 ),
+	('HD0009', 'DV0004', 4 ),
+	('HD0010', 'DV0003', 3 )
